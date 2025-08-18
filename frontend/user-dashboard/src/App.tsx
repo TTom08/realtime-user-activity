6b615f63de1091aef99243b6576c8f01899912bb
Tóth Tamás <t.tamas3208@gmail.com>
2025-08-18 14:38:34 +0200

    changing between pages working

--- frontend/user-dashboard/src/App.tsx
+++ frontend/user-dashboard/src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import Login from './components/Login';
 import Register from './components/Register'; 
+import "./App.css";
 
 function App() {
   return (
