257de6f60f1dca8216864ce29c618614f6470351
Tóth Tamás <t.tamas3208@gmail.com>
2025-08-28 11:23:11 +0200

    deleted unnecessary lines and comments from the backend

--- backend/activity-service/src/main/java/com/example/activity_service/service/KafkaConsumerService.java
+++ backend/activity-service/src/main/java/com/example/activity_service/service/KafkaConsumerService.java
@@ -6,23 +6,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaConsumerService {
-    //The KafkaConsumerService class listens for user login events from a Kafka topic
-    // and processes them by saving the activity using the ActivityService.
     private final ActivityService activityService;
 
-    // The constructor injects the ActivityService,
     public KafkaConsumerService(ActivityService activityService) {
         this.activityService = activityService;
     }
 
-    // The consumeUserLoggedInEvent method listens for messages on the "user-activity-topic"
-    // and processes UserLoggedInEvent messages.
-    // When a UserLoggedInEvent is received, it prints the user ID to the console
-    // and calls the saveActivity method of ActivityService to log the activity.
     @KafkaListener(topics = "user-activity-topic", groupId = "activity-service-group")
     public void consumeUserLoggedInEvent(UserActivityEvent event) {
         System.out.println("Received user logged in event from Kafka: " + event.getUserId());
-        // The saveActivity method of ActivityService is called to log the activity.
         activityService.saveActivity(event);
     }
 }
