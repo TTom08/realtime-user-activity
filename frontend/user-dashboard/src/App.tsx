75ae034566522933828d3b99e5db91e5d62f75d0
Zilahi Alexandra <zilahi.alexandra04@gmail.com>
2025-08-28 11:54:39 +0200

    Created Activities page

--- frontend/user-dashboard/src/App.tsx
+++ frontend/user-dashboard/src/App.tsx
@@ -1,18 +1,20 @@
 // src/App.tsx
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import Login from './components/Login';
-import Register from './components/Register'; 
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Activities from "./components/Activities";
 import "./App.css";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login/>} />
-        <Route path="/register" element={<Register/>} />
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/home" element={<HomePage />} />
+        <Route path="/" element={<Activities />} />
       </Routes>
     </Router>
   );
